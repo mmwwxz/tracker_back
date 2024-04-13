015ef41cf69ec56fa1f21b48cce95f44b08da6d7
muhammad613 <muhammad2808.a@mail.ru>
2024-04-13 20:28:23 +0600

    fix requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -133,12 +133,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 print("STATIC_ROOT is set to:", STATIC_ROOT)
