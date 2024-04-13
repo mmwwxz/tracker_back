ee78c7e396f8fb952de71db4c867d0bae16494c7
muhammad613 <muhammad2808.a@mail.ru>
2024-04-13 20:13:51 +0600

    fix requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 AUTH_USER_MODEL = 'account.CustomUser'
